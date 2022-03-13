7fc9cd227697caa920a6ede4d791b9cb09048f56
Revelation2106 <55440018+Revelation2106@users.noreply.github.com>
2022-03-13 17:15:41 +0000

    Updated some text

--- src/internal.h
+++ src/internal.h
@@ -743,7 +743,7 @@ struct _GLFWplatform
     void (*waitEventsTimeout)(double);
     void (*postEmptyEvent)(void);
 
-    // Hazel
+    // Spoonity
     void (*setWindowTitleBar)(_GLFWwindow*,GLFWbool);
 
     // EGL
